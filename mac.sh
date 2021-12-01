f8c41f76df96809f6f58caa7e60c964f636636ad
Collin Json <git@collin.fun>
2021-11-30 19:14:59 -0800

    Add header to mac file

--- mac.sh
+++ mac.sh
@@ -1,3 +1,5 @@
+# Mac initialization script
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install firefox visual-studio-code vlc steam bettertouchtool \
 github tnftp lastpass gimp discord 
