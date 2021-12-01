1a513da23352a5eef8d0a75b36e83f310a4eb09a
Collin Json <git@collin.fun>
2021-11-30 19:11:25 -0800

    Add basic mac init script

---
+++ mac.sh
@@ -0,0 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install firefox visual-studio-code vlc steam bettertouchtool \
+github tnftp lastpass gimp discord 
+
